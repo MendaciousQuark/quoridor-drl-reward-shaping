263f48c8bbde25e75dcef9a8726385e7b9e380a9
Jonathan Claussnitzer <ed20jicc@leeds.ac.uk>
2024-02-26 17:53:28 +0000

    Reverted and cleaned up main.py

--- src/main.py
+++ src/main.py
@@ -1,32 +1,8 @@
 from logic import playGame, initGameObjects
-from game import Board
 import pdb
 
 def main():
-    #playGame(*initGameObjects())
-    board = Board()
-    # #no path
-    # board.placeWall('horizontal', board.board[4][0])
-    # board.placeWall('horizontal', board.board[4][2])
-    # board.placeWall('horizontal', board.board[4][4])
-    # board.placeWall('horizontal', board.board[4][6])
-    # board.placeWall('horizontal', board.board[2][7])
-    # board.placeWall('vertical', board.board[2][7])
-    # board.placeWall('vertical', board.board[4][2])
-    # board.placeWall('vertical', board.board[4][3])
-    
-    #yes path
-    board.placeWall('horizontal', board.board[4][0])
-    board.placeWall('horizontal', board.board[4][2])
-    board.placeWall('horizontal', board.board[4][4])
-    board.placeWall('horizontal', board.board[4][6])
-    board.placeWall('horizontal', board.board[2][7])
-    board.placeWall('vertical', board.board[0][7])
-    board.placeWall('vertical', board.board[2][6])
-    board.placeWall('vertical', board.board[4][8])
-    
-    
-    print(board.printBoard())
+    playGame(*initGameObjects())
 
 
 if __name__ == '__main__':
