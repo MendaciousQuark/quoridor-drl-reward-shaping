f347e9405530c54ae63c2aab3e2cbdcd1a44f770
Jonathan Claussnitzer <ed20jicc@leeds.ac.uk>
2024-05-10 17:16:51 +0100

    Added flow for letting all existing trained models compete

--- src/main.py
+++ src/main.py
@@ -32,9 +32,7 @@ def graphing(args):
 
 def main():
     parser = argparse.ArgumentParser(description='Train or play the game.')
-    parser.add_argument('--mode', choices=['train', 'play', 'ground-truth', 'baseline', 'diversity'], default='train', help='Mode to run the game. Choices are "train", "play", "baseline", "diversity" or "ground-truth". Default is "train".')
-
-    parser.add_argument('--mode', choices=['train', 'play', 'ground_truth', 'train_baseline'], default='train', help='Mode to run the game. Choices are "train", "play" or "ground_truth", "train_baseline". Default is "train".')
+    parser.add_argument('--mode', choices=['train', 'play', 'ground-truth', 'baseline', 'diversity', "compete-all"], default='train', help='Mode to run the game. Choices are "train", "play", "baseline", "compete-all", "diversity" or "ground-truth". Default is "train".')
     
     #arguments for train mode
     parser.add_argument('--with_ground_truths', action='store_true', help='Train with ground truths. Default is False. Note: This argument is only used in train mode.')
@@ -80,6 +78,21 @@ def main():
         from game.game import Game
         game = Game()
         game.play(args.colour, args.game_mode)
+    elif args.mode == 'compete-all':
+        from utils.utils import find_agent_paths
+        from models.training_setup import init_agents
+        from tournament.swiss_tournament import SwissTournament
+        from utils.create_agents import create_agents
+        import math
+        
+        white_agent_paths = find_agent_paths("src", "white_agents")
+        black_agent_paths = find_agent_paths("src", "black_agents")
+        
+        white_agents = create_agents(white_agent_paths, "white")
+        black_agents = create_agents(black_agent_paths, "white")
+        tournament = SwissTournament(white_competitors=white_agents, black_competitors=black_agents)
+        tournament.compete(num_rounds = math.ceil(math.log2(len(white_agents))))
+
     elif args.mode == 'ground_truth':
         from utils.create_groundtruth import creatGroundTruth
         for _ in range(args.num):
